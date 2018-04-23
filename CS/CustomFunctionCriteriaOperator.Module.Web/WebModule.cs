7f060829d6500f9403b1a59d95e285807e1f5b3a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomFunctionCriteriaOperator.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace CustomFunctionCriteriaOperator.Module.Web {
+   [ToolboxItemFilter("Xaf.Platform.Web")]
+   public sealed partial class CustomFunctionCriteriaOperatorAspNetModule : ModuleBase {
+      public CustomFunctionCriteriaOperatorAspNetModule() {
+         InitializeComponent();
+      }
+   }
+}
